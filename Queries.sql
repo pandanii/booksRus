46813bb6873264c4d3a4bb28a9d755b4369533ac
Danielle Lewis <danilynnlewis@gmail.com>
2016-11-05 17:57:22 -0400

    A few of the search queries
    created a few of the search queries:
    - title search for dvds
    - title search for books
    - directors search
    - authors search
    
    TODO:
    - need to find a way to put in top sellers, will do later once DB is
    completed.
    - need to upload exported DB I have created with test values.

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,31 @@
+/*DVD Title search*/ /*WORKS*/ /*NEEDS TOP SELLER!!!!*/
+SELECT media.title as "DVD Title" , media.year as "YEAR RELEASED", media.price, media.copies_In_Stock as "Amount in stock", dvds.cast, dvds.director, dvds.genre 
+FROM dvds, media
+WHERE media.title = dvds.title AND media.title like "catdog";
+/*replace "catdog" with ? in java program.*/
+/*EndOf Director Search*/
 
-//* Director search*//
-SELECT *
-FROM 
-WHERE
+/************************************************************************************************************************************************************************************************************************/
+/*Books Title search*/ /*WORKS*/ /*NEEDS TOP SELLER!!!!*/
+SELECT media.title as "DVD Title" , media.year as "YEAR RELEASED", media.price, media.copies_In_Stock as "Amount in stock", dvds.cast, dvds.director, dvds.genre 
+FROM dvds, media
+WHERE media.title = dvds.title AND media.title like "catdog";
+/*replace "catdog" with ? in java program.*/
+/*EndOf Director Search*/
+/************************************************************************************************************************************************************************************************************************/
+
+/*Director Search*/ /*WORKS*/ /*NEEDS TOP SELLER!!!!*/
+SELECT media.title as "Book Title" , media.year as "YEAR RELEASED", media.price, media.copies_In_Stock as "Amount in stock" , books.ISBN, books.subject_category,books.name as "Publisher", authors.name as "Author Name"
+FROM books, written_by, authors, media
+WHERE media.title = books.title AND authors.name = written_by.name AND authors.address = written_by.address AND media.title = "WitchCraft and Wizardry";
+/*replace "WitchCraft and Wizardry"" with ? in java program.*/
+/*EndOf Director Search*/
+/************************************************************************************************************************************************************************************************************************/
+
+/*Authors Search*/ /*NEEDS TOP SELLER!!!!*/
+SELECT media.title as "Book Title" , media.year as "YEAR RELEASED", media.price, media.copies_In_Stock as "Amount in stock" , books.ISBN, books.subject_category,books.name as "Publisher", authors.name as "Author Name"
+FROM books, written_by, authors, media
+WHERE media.title = books.title AND authors.name = written_by.name AND authors.address = written_by.address AND authors.name = "fred";
+/*replace "fred" with ? in java program.*/
+/*EndOf Authors Search*/
+/************************************************************************************************************************************************************************************************************************/
