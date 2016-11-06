72e4bf166930d7575cc721ef5ae1d3dc7038b1ff
Aujulyn <maxxkingdom@gmail.com>
2016-11-06 17:52:19 -0500

    Add files via upload

--- src/Queries.java
+++ src/Queries.java
@@ -1,100 +1,83 @@
 /*
- *This class is designed to hold the queries for the different actions performed by the database.
- *Storing them this way allows for cleaner looking code.
- */
-public class Queries 
+*This class is designed to hold the queries for the different actions performed by the database.
+*/
+public class Queries
 {
-    
-    public final String displayResultsDVDs;
-    public final String displayResultsBooks;
-    
-    
     public final String title_DVDs_Search;
     public final String director_Search;
     public final String cast_Search;
     public final String genre_Search;
-    public final String sequel_Search;
+    //public final String sequel_Search;
     public final String keyword_DVDs_Search;
-    
+
     public final String title_Books_Search;
-    public final String author_Search ;
-    public final String publisher_Search;
-    public final String subject_Cate_Search;
-    public final String keyword_Books_Search;
-    
-    
+    //public final String author_Search;
+    //public final String publisher_Search;
+    //public final String subject_category_Search;
+
+
     public Queries()
     {
-        
-
-               displayResultsDVDs = "SELECT DISTINCT * "+
-                                    "FROM dvds ar " +
-                                    "WHERE ar.rid IN ("; 
-        
-        
-        
-        
-      /*
-       *!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       *Theses queries are setup to just give us the media.title as the return.
-       *An alternative query will display the results in the method we want.
-       *!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       */
        /* DVDS SEARCH STUFF */
-       title_DVDs_Search   = "SELECT DISTINCT media.title" 
-                           + "FROM dvds, media" 
+       title_DVDs_Search   = "SELECT DISTINCT media.title"
+                           + "FROM dvds, media"
                            + "WHERE media.title = dvds.title AND media.title like ?";
-       
-       director_Search     = "SELECT DISTINCT media.title"  
-                           + "FROM dvds, media" 
+
+       director_Search     = "SELECT DISTINCT media.title"
+                           + "FROM dvds, media"
                            + "WHERE media.title = dvds.title AND  dvds.director like ?";
-       
-       cast_Search         = "SELECT DISTINCT media.title"  
-                           + "FROM dvds, media" 
+
+       cast_Search         = "SELECT DISTINCT media.title"
+                           + "FROM dvds, media"
                            + "WHERE media.title = dvds.title AND  dvds.cast like ?";
-        
+
        genre_Search        = "SELECT DISTINCT media.title"
                            + "FROM dvds, media"
                            + "WHERE media.title = dvds.title AND  dvds.genre like ?";
-       
-       sequel_Search       = "SELECT DISTINCT sequel_to.sequel_title" 
-                           + "FROM dvds, media, sequel_to"
-                           + "WHERE media.title = ? AND media.title = dvds.title AND  dvds.title = sequel_to.prequel_title"; 
-       
-       keyword_DVDs_Search = title_DVDs_Search 
-                           + " UNION " 
-                           + director_Search 
-                           + " UNION " 
-                           + cast_Search 
-                           + " UNION " 
+
+       keyword_DVDs_Search = title_DVDs_Search
+                           + " UNION "
+                           + director_Search
+                           + " UNION "
+                           + cast_Search
+                           + " UNION "
                            + genre_Search;
-       
-       
+
        /* BOOKS SEARCH STUFF */
        title_Books_Search  = "SELECT DISTINCT media.title "
-                           + "FROM books, media"
+                           + "FROM books, written_by, authors, media"
                            + "WHERE media.title = books.title  AND books.title like ?";
-       
-       author_Search       = "SELECT DISTINCT media.title"
-                           + "FROM books, written_by, authors, media" 
-                           + "WHERE media.title = books.title AND authors.name = written_by.name AND authors.address = written_by.address AND authors.name like ?";
-       
-       publisher_Search    = "SELECT DISTINCT media.title" 
-                           + "FROM books, media"
-                           + "WHERE media.title = books.title  AND name like ?";      
-               
-       subject_Cate_Search = "SELECT DISTINCT media.title" 
-                           + "FROM books, media"
-                           + "WHERE media.title = books.title  AND books.subject_category like ?";      
-               
-       keyword_Books_Search = title_Books_Search 
-                           + " UNION " 
-                           + author_Search 
-                           + " UNION " 
-                           + publisher_Search 
-                           + " UNION " 
-                           + subject_Cate_Search;
-       
-       
-    }//EndOf Queries constructor 
+    }//EndOf Queries constructor
+    //=====================================================
+    public String getQuery(String queryName)
+    {
+        if (queryName.equals("title_DVDs_Search"))
+        {
+            return title_DVDs_Search;
+        }
+        else if (queryName.equals("director_Search"))
+        {
+            return director_Search;
+        }
+        else if (queryName.equals("cast_Search"))
+        {
+            return cast_Search;
+        }
+        else if (queryName.equals("genre_Search"))
+        {
+            return genre_Search;
+        }
+        else if (queryName.equals("keyword_DVDs_Search"))
+        {
+            return keyword_DVDs_Search;
+        }
+        else if (queryName.equals("title_Books_Search"))
+        {
+            return title_Books_Search;
+        }
+        else
+        {
+            return "";
+        }
+    }
 }//EndOf Queries class.
