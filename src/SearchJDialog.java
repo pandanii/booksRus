4f5edbba3bdce8aafedc928ea2700c6f0908e271
Aujulyn <maxxkingdom@gmail.com>
2016-11-06 20:16:01 -0500

    Add files via upload

--- src/SearchJDialog.java
+++ src/SearchJDialog.java
@@ -67,7 +67,12 @@ JComboBox searchComboBox;
     searchComboBox.addItem("Cast Member Name");
     searchComboBox.addItem("Genre");
     searchComboBox.addItem("DVD Keyword");
+    searchComboBox.addItem("Sequel");
     searchComboBox.addItem("Book Title");
+    searchComboBox.addItem("Author Name");
+    searchComboBox.addItem("Publisher Name");
+    searchComboBox.addItem("Book Catagory");
+    searchComboBox.addItem("Book Keyword");
 
     searchTextField = new JTextField(30);
     searchTextField.getDocument().addDocumentListener(this);
@@ -119,55 +124,102 @@ JComboBox searchComboBox;
         {
         PreparedStatement preparedStatement;
         Object comboObject;
-        String queryString;
         String searchString;
-
+        Connection connection;
         ResultSet resultSet;
+
+        connection = pointerToStoreFrame.connection;
 
         searchString = searchTextField.getText().trim();    //get what the user wants to search for
 
         comboObject = searchComboBox.getSelectedItem();     //get what they want to search in from the combo box
                                                             //and relate it to the Queries data members
-        queryString = "";
-        if (comboObject.toString().equals("DVD Title"))
-            {
-            queryString = "title_DVDs_Search";
-            }
-        else if (comboObject.toString().equals("Director Name"))
-            {
-            queryString = "director_Search";
-            }
-        else if (comboObject.toString().equals("Cast Member Name"))
-            {
-            queryString = "cast_Search";
-            }
-        else if (comboObject.toString().equals("Genre"))
-            {
-            queryString = "genre_Search";
-            }
-        else if (comboObject.toString().equals("DVD Keyword"))
-            {
-            queryString = "keyword_DVDs_Search";
-            }
-        else if (comboObject.toString().equals("Book Title"))
-            {
-            queryString = "title_Books_Search";
-            }
-
-
+        resultSet = null;
         try
             {
-            Connection connection;
-            connection = pointerToStoreFrame.connection;    //just to shorten the next line of code up.
-            preparedStatement = connection.prepareStatement(listOfQueries.getQuery(queryString));
-            //to avoid having duplicate code inside multiple if's, I added a method to Queries
-            preparedStatement.clearParameters();
-            preparedStatement.setString(1, searchString);   //this format will have to change should we ever need to run queries with
-                                                            //multiple '?' in them
 
-            resultSet = preparedStatement.executeQuery();
+            if (comboObject.toString().equals("DVD Title"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.title_DVDs_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Director Name"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.director_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Cast Member Name"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.cast_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Genre"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.genre_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("DVD Keyword"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.keyword_DVDs_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Sequel"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.sequel_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Book Title"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.title_Books_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Author Name"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.author_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Publisher Name"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.publisher_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Book Category"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.subject_Cate_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
+            else if (comboObject.toString().equals("Book Keyword"))
+                {
+                preparedStatement = connection.prepareStatement((String)listOfQueries.keyword_Books_Search);
+                preparedStatement.clearParameters();
+                preparedStatement.setString(1, searchString);
+                resultSet = preparedStatement.executeQuery();
+                }
 
-            pointerToStoreFrame.updateResultTable(resultSet);
+            if (resultSet != null)
+                {
+                pointerToStoreFrame.updateResultTable(resultSet);   //sending the resultSet to StoreFrame to be displayed
+                }
             }
         catch (SQLException sqle)
             {
