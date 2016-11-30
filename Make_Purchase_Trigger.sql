4bb54b22909fed49944f03ef45bebb90eddd6d7b
Danielle Lewis <danilynnlewis@gmail.com>
2016-11-30 18:34:18 -0500

    Should have went through with last update

--- Make_Purchase_Trigger.sql
+++ Make_Purchase_Trigger.sql
@@ -1,8 +1,11 @@
 DELIMITER $$
+CREATE TRIGGER beforeInsertOnPurchase BEFORE INSERT ON purchase
 FOR EACH ROW BEGIN
+	CASE(SELECT copies_In_Stock FROM media WHERE title = NEW.title)
 		WHEN(0) THEN
 			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Current item is out of stock';
 		ELSE
+			UPDATE media
 			SET    media.copies_In_Stock = media.copies_In_Stock - 1
 			WHERE  media.title = NEW.title;
 	END CASE;
